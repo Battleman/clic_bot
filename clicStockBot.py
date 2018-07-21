05f85c0e06dda0354bef392f3b3ea945f531c845
Olivier Cloux <olivier.cloux@epfl.ch>
2018-07-21 11:35:27 +0200

    refractoring to follow pylint and pep8 guidelines

--- clicStockBot.py
+++ clicStockBot.py
@@ -1,31 +1,40 @@
-#!/home/battleman/Programs/anaconda3/bin/python
+"""
+ClicBot.py
+Author: Olivier Cloux
+This module allows our student association (the Clic) to manage its stock, and 
+more
+"""
 import itertools
-import json
+# import json
 import logging
+# import subprocess
 import sys
+import time
+from datetime import datetime, timedelta
 from os.path import isfile
 from pprint import pprint
-from dateutil.parser import parse
+
+import schedule
 import telegram
 import yaml
-import schedule
-import time
 from apiclient.discovery import build
+from dateutil.parser import parse
 from httplib2 import Http
 from oauth2client import client, file, tools
-from telegram.utils.request import Request
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
+from telegram.utils.request import Request
 
-"""
-#	Load the config file
-#	Set the Botname / Token
-"""
+# ########
+# Load the config file
+# Set the Botname / Token
+# ##########
 config_file = 'config.yaml'
 if isfile(config_file):
     with open(config_file) as fp:
         config = yaml.load(fp)
 else:
-    pprint('config.yaml file does not exists. Please make from config.sample.yaml file')
+    pprint("config.yaml file does not exists. Please make from "
+           "config.sample.yaml file")
     sys.exit()
 
 #####################
@@ -51,14 +60,19 @@ NUM_COL_NAME = config['NUM_COL_NAME']
 NUM_COL_QTY = config['NUM_COL_QTY']
 NUM_COL_UNIT = config['NUM_COL_UNIT']
 NUM_COL_EXPIRY = config['NUM_COL_EXPIRY']
+SUBSCRIBED_EXPIRY_FILENAME = config['SUBSCRIBED_EXPIRY_FILENAME']
 
 commands = {'list': 'List all items in stock',
             'help': 'This command help :)',
             # 'quit': 'stop the bot. For everyone. Forever.',
-            'identify': 'Let the owner know your TG ID. Only useful for admins.',
+            'identify': 'Let the owner know your TG ID.'
+            ' Only useful for admins.',
             'new': 'Add an item to the stock',
             'search': 'Enter a string to search for in the stock',
-            'update': 'Update an existing entry in the stock'
+            'update': 'Update an existing entry in the stock',
+            'subscribe': "Subscribe to the expiry messaging list. "
+            "Every day at 12h00, you'll receive a message about"
+            " the next and currently  expired items"
             }
 
 
@@ -68,7 +82,7 @@ commands = {'list': 'List all items in stock',
 FORMAT = '%(asctime)s -- %(levelname)s -- %(module)s %(lineno)d -- %(message)s'
 logging.basicConfig(level=logging.INFO, format=FORMAT)
 logger = logging.getLogger('root')
-logger.info("Running "+sys.argv[0])
+logger.info("Running %s", sys.argv[0])
 
 
 ############################
@@ -78,15 +92,18 @@ def tg_start(bot, update):
     bot.send_message(chat_id=update.message.chat_id,
                      text="I'm a bot, please talk to me!")
 
+
 def tg_quit(bot, update):
     if update.message.chat_id != OWNER_ID:
         bot.send_message(chat_id=update.message.chat_id,
-                         text="Sorry, you can't perform this task. You're not an admin.")
+                         text="Sorry, you can't perform this task. You're not "
+                         "an admin.")
     else:
         bot.send_message(chat_id=update.message.chat_id,
                          text="Thanks for talking with me. By by !")
         updater.stop()
         sys.exit()
+
 
 def tg_listItems(bot, update):
     values = gs_getAllValues()
@@ -95,33 +112,43 @@ def tg_listItems(bot, update):
     l = values[1:]
     pprint_tg(bot, update, l, header)
 
-def pprint_tg(bot, update, l, header=""):
-    titre = "Object, Quantity, Unit, Expiry Date\n"
-    # content = "\n\n".join(["{}\t\t{}\t\t{}".format(v[0], v[1], v[2]) if len(v) == 3
-                        #    else "{}\t\t{}".format(v[0], v[1]) for v in l[1:] if len(v) > 1])
+
+def pprint_tg(bot, chat_id,
+              l,
+              header="The following items are in stock:\n",
+              columns=config['COLS_NAMES']):
     line = "{},\t\t"*(NUM_COLS-1) + "{}"
-    content = "\n\n".join([line.format(elem[NUM_COL_NAME], elem[NUM_COL_QTY], elem[NUM_COL_UNIT], elem[NUM_COL_EXPIRY]) for elem in l])                   
+    content = "\n\n".join([line.format(elem[NUM_COL_NAME],
+                                       elem[NUM_COL_QTY],
+                                       elem[NUM_COL_UNIT],
+                                       elem[NUM_COL_EXPIRY]) for elem in l])
     # content+="\n\n"+"\n\n".join([)
-    s = titre + content
-    bot.send_message(chat_id=update.message.chat_id,
+    s = header + content
+    bot.send_message(chat_id=chat_id,
                      text="The following articles are in stock:\n{}".format(s))
+
 
 def tg_unknown(bot, update):
     bot.send_message(chat_id=update.message.chat_id,
                      text="Sorry, I didn't understand that command.")
 
+
 def tg_helper(bot, update):
-    helpHeader = "Hi! This is the help message for ClicStock_Bot. Here are the various commands you can use:\n\n"
+    helpHeader = "Hi! This is the help message for ClicStock_Bot. \
+    Here are the various commands you can use: \n\n"
     helpBody = "\n".join(["{}\t\t{}".format(x, commands[x]) for x in commands])
     bot.send_message(chat_id=update.message.chat_id, text=helpHeader+helpBody)
+
 
 def tg_echo(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
 
+
 def tg_addItem(bot, update, args):
     newObj = parseListCommas(args)
     if len(newObj) < MIN_NUM_COLS or len(newObj) > NUM_COLS:
-        message = "You need to specify the object, quantity, unit and optionally the expiry date (commas separated), no more"
+        message = "You need to specify the object, quantity, unit and \
+        optionally the expiry date (commas separated), no more"
         bot.send_message(chat_id=update.message.chat_id, text=message)
         return
 
@@ -130,9 +157,10 @@ def tg_addItem(bot, update, args):
         newObj[NUM_COL_QTY] = float(newObj[NUM_COL_QTY])
         assert newObj[NUM_COL_QTY] >= 0
     except (ValueError, AssertionError):
-        message = "Your second item (quantity) could not be understood as a positive number. Please check."
-        logger.warn('Chat id {} tried to add quantity {}'.format(
-            update.message.chat_id, newObj[NUM_COL_QTY]))
+        message = "Your second item (quantity) could not be understood \
+        as a positive number. Please check."
+        logger.warning('Chat id %d tried to add quantity %s',
+                       update.message.chat_id, newObj[NUM_COL_QTY])
         bot.send_message(chat_id=update.message.chat_id, text=message)
         return
 
@@ -141,9 +169,10 @@ def tg_addItem(bot, update, args):
         try:
             newObj[NUM_COL_EXPIRY] = parse(newObj[NUM_COL_EXPIRY])
         except ValueError:
-            message = "Your last item (expiration date) could not be understood as a date. Please check."
-            logger.warn('Chat id {} tried to add expire date {}'.format(
-                update.message.chat_id, newObj[NUM_COL_EXPIRY]))
+            message = "Your last item (expiration date) could not \
+            be understood as a date. Please check."
+            logger.warning('Chat id %s tried to add expire date %s',
+                           update.message.chat_id, newObj[NUM_COL_EXPIRY])
             bot.send_message(chat_id=update.message.chat_id, text=message)
             return
     else:  # if no date specified, add NA
@@ -161,14 +190,17 @@ def tg_addItem(bot, update, args):
             message = "Failed to add... please refer to an admin"
     bot.send_message(chat_id=update.message.chat_id, text=message)
 
+
 def tg_getChatID(bot, update, args):
-    id = update.message.chat_id
+    user_id = update.message.chat_id
     if len(args) > 0:
         print("User with ID {} just identified himself as {}".format(
-            id, args[0]))
+            user_id, args[0]))
     else:
-        print("User with ID {} just identified himself anonymously".format(id))
-    return id
+        logger.info("User with ID %s just identified himself anonymously",
+                    user_id)
+    return user_id
+
 
 def tg_searchItem(bot, update, args):
     args = parseListCommas(args)
@@ -183,8 +215,9 @@ def tg_searchItem(bot, update, args):
     for i in items[1:]:
         if i[NUM_COL_NAME].lower().find(word.lower()) >= 0:
             result += [i]
-    
+
     pprint_tg(bot, update, result)
+
 
 def tg_updateValue(bot, update, args):
     # Arguments parsing
@@ -192,7 +225,8 @@ def tg_updateValue(bot, update, args):
     parsed = parseListCommas(args)
     if len(parsed) < MIN_NUM_COLS_UPDATE:
         bot.send_message(chat_id=update.message.chat_id,
-                         text="You did not specify enough arguments. Specify at least Item and Quantity (comma separated)")
+                         text="You did not specify enough arguments. Specify "
+                         "at least Item and Quantity (comma separated)")
         return
 
     obj = parsed[0].lower()
@@ -213,14 +247,17 @@ def tg_updateValue(bot, update, args):
     rawValues = gs_getAllValues()
     values = list(itertools.zip_longest(*rawValues))
     print(values)
-    index = (i for i, v in enumerate(values[0]) if v.lower() == obj)
-    try:
-        objPos = next(index)
-    except StopIteration:
+    index = [i for i, v in enumerate(values[0]) if v.lower() == obj]
+    if(len(index) == 0):
         bot.send_message(chat_id=update.message.chat_id,
                          text=config['ERROR_UPDATE_404'].format(obj))
         return
-
+    elif(len(index > 1)):
+        header = config['ERROR_UPDATE_TOO_MANY_MATCH']
+        names = index  # TODO keep only names
+        pprint_tg(bot, update.message.chat_id, names, header)
+        return
+    objPos = index[0][0]  # TODO Check this is correct
     # find new value according to relativity
     newVal = quantity
     if relative:
@@ -234,11 +271,87 @@ def tg_updateValue(bot, update, args):
         bot.send_message(chat_id=update.message.chat_id,
                          text=config['UPDATE_NOT_OK'])
 
-def check_expiry():
-    logger.info("The cron works !")
 
+def tg_subscribe_expiry(bot, update):
+    with open(SUBSCRIBED_EXPIRY_FILENAME, mode='r+') as f:
+        for i in f:
+            if i.strip() == str(update.message.chat_id):
+                message = "Sorry, you are already subscribed to this list."
+                bot.send_message(chat_id=update.message.chat_id,
+                                 text=message)
+                return
+        # if we did not return, we are at the end of the file, we can append
+        ret = f.write(str(update.message.chat_id)+"\n")
+        if ret > 0:
+            message = "You subscribes to this list! Congratulations"
+        else:
+            message = "Something wrong happened... contact an admin"
+        bot.send_message(chat_id=update.message.chat_id,
+                         text=message)
+
+
+def tg_unsubscribe_expiry(bot, update):
+    lines = []
+    with open(SUBSCRIBED_EXPIRY_FILENAME, mode="r") as f:
+        found = False
+        for i in f:
+            if not str(update.message.chat_id) in i:
+                lines.append(i)
+            else:
+                found = True
+    if found:
+        with open(SUBSCRIBED_EXPIRY_FILENAME, mode="w") as f:
+            f.writelines(lines)
+        message = "You were correctly unsubscribed."
+    else:
+        message = "You are not currently subscribed. \
+        Use /subscribe to subscribe."
+
+    bot.send_message(chat_id=update.message.chat_id, text=message)
+
+
+def check_expiry(bot, update=None):
+
+    today = datetime.today()
+    vals = gs_getAllValues()[1:]
+    # keeps values in next 7 days
+    sensible = [v for v in vals if ('NA' not in v[NUM_COL_EXPIRY]) and
+                (parse(v[NUM_COL_EXPIRY]) - timedelta(days=7) < today)]
+    stillGood = [v for v in sensible if parse(v[NUM_COL_EXPIRY]) >= today]
+    expired = [v for v in sensible if parse(v[NUM_COL_EXPIRY]) < today]
+
+    line = "-->"+"{},\t\t"*(NUM_COLS-1) + "{}"
+    message = ""
+    if len(stillGood) == 0:
+        message += "No good will be expired in the next 7 days\n###########\n"
+    else:
+        message += "The following good(s) will be expired in the next week:\n"
+        message += "\n".join([line.format(elem[NUM_COL_NAME],
+                                          elem[NUM_COL_QTY],
+                                          elem[NUM_COL_UNIT],
+                                          elem[NUM_COL_EXPIRY])
+                              for elem in stillGood])
+        message += "\n###########\n"
+    if len(expired) == 0:
+        message += "No good is currently expired\n###########\n"
+    else:
+        message += "The following good(s) already expired. SHAME !:\n"
+        message += "\n".join([line.format(elem[NUM_COL_NAME],
+                                          elem[NUM_COL_QTY],
+                                          elem[NUM_COL_UNIT],
+                                          elem[NUM_COL_EXPIRY])
+                              for elem in expired])
+        message += "\n###########\n"
+    if update is None:
+        with open(SUBSCRIBED_EXPIRY_FILENAME, mode='r') as f:
+            for user_id in f:
+                bot.send_message(chat_id=user_id, text=message)
+    else:
+        bot.send_message(chat_id=update.message.chat_id, text=message)
 ########################
 #   Google Sheet functions
+
+
 def gs_appendValue(value):
     service = gs_getService()
     print(len(value))
@@ -251,10 +364,10 @@ def gs_appendValue(value):
             value
         ]
     }
-    range = "Stock!A2:C2"
+    sheet_range = "Stock!A2:C2"
     response = service.spreadsheets().values().append(
         spreadsheetId=SPREADSHEET_ID,
-        range=range,
+        sheet_range=sheet_range,
         body=resource,
         valueInputOption="USER_ENTERED"
     ).execute()
@@ -264,12 +377,12 @@ def gs_appendValue(value):
 def gs_getService():
     """ Setup the Sheets API"""
     store = file.Storage('credentials.json')
-    creds = store.get()
-    if not creds or creds.invalid:
+    credentials = store.get()
+    if not credentials or credentials.invalid:
         print("Credentials invalid, renewing")
         flow = client.flow_from_clientsecrets('client_secret.json', SCOPE)
-        creds = tools.run_flow(flow, store)
-    service = build('sheets', 'v4', http=creds.authorize(
+        credentials = tools.run_flow(flow, store)
+    service = build('sheets', 'v4', http=credentials.authorize(
         Http()), cache_discovery=False)
     return service
 
@@ -307,8 +420,9 @@ def gs_getValuesFromResponse(response):
 
 
 def gs_getAllValues():
-    result = gs_getService().spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID,
-                                                         range=VALUES_RANGE_COLS).execute()
+    result = gs_getService().spreadsheets().values().get(
+        spreadsheetId=SPREADSHEET_ID,
+        range=VALUES_RANGE_COLS).execute()
     values = result.get('values', [])
     if not values:
         print('No data found.')
@@ -351,8 +465,8 @@ request = Request(con_pool_size=8)
 clicBot = telegram.Bot(token=TOKEN, request=request)
 updater = Updater(bot=clicBot)
 
-# schedule.every().day.at("12:00").do(check_expiry())
-schedule.every(1).minutes.do(check_expiry)
+# schedule.every().day.at("12:00").do(check_expiry, clic_bot)
+# schedule.every(1).minutes.do(check_expiry, clicBot)
 
 
 dispatcher = updater.dispatcher
@@ -374,6 +488,13 @@ dispatcher.add_handler(CommandHandler(
     'identify', tg_getChatID, pass_args=True))
 dispatcher.add_handler(CommandHandler(
     'search', tg_searchItem, pass_args=True))
+dispatcher.add_handler(CommandHandler(
+    'subscribe', tg_subscribe_expiry))
+dispatcher.add_handler(CommandHandler(
+    'unsub', tg_unsubscribe_expiry))
+
+dispatcher.add_handler(CommandHandler(
+    'test', check_expiry))
 
 # dispatcher.add_handler(MessageHandler(Filters.text, tg_echo))
 dispatcher.add_handler(MessageHandler(Filters.command, tg_unknown))
@@ -383,6 +504,7 @@ updater.start_polling()
 
 ######################
 #       UTILS
+
 
 def parseListCommas(l):
     """
@@ -409,4 +531,3 @@ def parseListCommas(l):
 while 1:
     schedule.run_pending()
     time.sleep(30)
-
